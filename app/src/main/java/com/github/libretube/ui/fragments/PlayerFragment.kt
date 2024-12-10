65c4dda9e560906956081e58ffc28d932f55a613
FineFindus <FineFindus@proton.me>
2024-12-10 20:43:41 +0100

    feat(PlayerFragment): cap predictive back progress
    Sets a limit for the maximum progress of the predictive back gesture in
    the PlayerFragment. Before reaching the maximum progress, the amound of
    displayed progress is gradually smooth out, such that further progress
    appears less.
    This makes the back gesture behave more like the one used by the
    bottom-sheets.
    Ref: f143e0d89430b7dd10aff091a52d125694af6e22

--- app/src/main/java/com/github/libretube/ui/fragments/PlayerFragment.kt
+++ app/src/main/java/com/github/libretube/ui/fragments/PlayerFragment.kt
@@ -116,6 +116,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import kotlin.math.abs
 import kotlin.math.ceil
+import kotlin.math.pow
 
 
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
@@ -478,7 +479,8 @@ class PlayerFragment : Fragment(R.layout.fragment_player), OnlinePlayerOptions {
             }
 
             override fun handleOnBackProgressed(backEvent: BackEventCompat) {
-                binding.playerMotionLayout.progress = backEvent.progress
+                val smoothedProgress = MAX_PROGRESS * (1 - 2f.pow(-backEvent.progress / MAX_PROGRESS * 5f))
+                binding.playerMotionLayout.progress = smoothedProgress
             }
 
             override fun handleOnBackCancelled() {
@@ -1545,5 +1547,9 @@ class PlayerFragment : Fragment(R.layout.fragment_player), OnlinePlayerOptions {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val MAX_PROGRESS = 0.4f
     }
 }
